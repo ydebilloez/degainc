bdb32bce0aba6137a8f96ecb830560b480ba6789
Yves <yveslub@gmail.com>
2024-02-19 12:30:40 +0200

    Operations - step 1

--- public_html/operations.php
+++ public_html/operations.php
@@ -3,6 +3,15 @@ include(dirname(__FILE__).'/phpMyEditHeader.php');
 ?>
 
 <?php
+function phpMyEditPageHeader($inst) {
+    if (in_array($inst->{'page_type'}, array('C'))) {
+        // only allow comments to be modified
+        $inst->{'fdd'}['commande_id']['options'] = 'ACVDFR';
+        $inst->{'fdd'}['value_operation']['options'] = 'ACVDFR';
+        $inst->{'fdd'}['date_operation']['options'] = 'ACVDFR';
+        $inst->recreate_displayed();
+    }
+}
 
 /*
  * IMPORTANT NOTE: This generated file contains only a subset of huge amount
@@ -23,7 +32,7 @@ include(dirname(__FILE__).'/phpMyEditHeader.php');
  * This file was NOT manually updated.
  */
 
-require_once(dirname(__FILE__).'/lib/phpMyEdit.class.php');
+require_once(dirname(__FILE__).'/lib/extensions/phpMyEdit-multiquery.class.php');
 require_once(dirname(__FILE__).'/lib/phpMyEditDB.php');
 require_once(dirname(__FILE__).'/phpMyEditDefaults.php');
 
@@ -33,15 +42,18 @@ $opts['tb'] = 'operations';
 $opts['cgi']['prefix']['data'] = 'operations_';
 $opts['cgi']['persist'] = array('oper' => $_REQUEST['oper']);
 $operation = $opts['cgi']['persist']['oper'];
+$opts['options'] = 'ACVDF';
 
 if ($operation == 'Vente') {
     $opts['filters'] = "`commande_id` in (select `commandes`.`rowid` from `commandes` where `commandes`.`co_type` = 'Vente')"; //AND `date_paiement` IS NULL
     $filter = "`co_type` = 'Vente'";
+    $transTitle = 'Paiement reçu';
     $title = 'Paiment par client';
 } else {
     // operation = Livraison
     $opts['filters'] = "`commande_id` in (select `commandes`.`rowid` from `commandes` where `commandes`.`co_type` = 'Achat')"; //AND `date_paiement` IS NULL
     $filter = "`co_type` = 'Achat'";
+    $transTitle = 'Paiement effectué';
     $title = 'Reglement fournisseur';
 }
 
@@ -72,25 +84,27 @@ $opts['fdd']['commande_id'] = array(
        'maxlen' => '10',
        'values' => array('table'  => 'commandes',
                          'column' => 'rowid',
-                         'description' => array('columns' => array('date_commande', 'pa_code', 'commentaires'),
-                                                'divs'    => array (' - ', ' - ')),
+                         'description' => array('columns' => array('date_commande', 'pa_code', 'prixtotal', 'commentaires'),
+                                                'divs'    => array (' - ', ' - ', '$ - ')),
                          'filters' => $filter
                         ),
          'sort' => true
 );
+$opts['fdd']['value_operation'] = array(
+         'name' => $transTitle,
+       'select' => 'N',
+       'maxlen' => '10',
+      'default' => '',
+           'js' => array('required' => true),
+         'help' => '$'
+);
 $opts['fdd']['date_operation'] = array(
-         'name' => 'Date ' . $operation,
+         'name' => 'Date paiement',
        'select' => 'T',
        'maxlen' => '10',
       'default' => date('Y-m-d'),
            'js' => array('required' => true),
          'sort' => true
-);
-$opts['fdd']['value_operation'] = array(
-         'name' => 'Montant ' . $operation,
-       'select' => 'N',
-       'maxlen' => '10',
-      'default' => '0'
 );
 $opts['fdd']['commentaires'] = array(
          'name' => 'Commentaires',
@@ -98,33 +112,15 @@ $opts['fdd']['commentaires'] = array(
        'maxlen' => '255'
 );
 
-// possibly initialise page further before going to main function
-
-if (function_exists('phpMyEditHeaderInit')) { phpMyEditHeaderInit($opts); }
-
-// now copy php variables over to js variables
-// protect sensitive variables so they cannot be read
-$cleanopts = $opts;
-unset($cleanopts['hn']); unset($cleanopts['pt']);
-unset($cleanopts['un']); unset($cleanopts['pw']);
-
 echo '
 <script>
-    var phpOpts = ' . json_encode($cleanopts) . ';
-    try {
-        if (typeof PME_js_init === \'function\') {
-            PME_js_init(phpOpts);
-        }
-    } catch(err) {
-        console.log(err);
-    }
     PME_js_setPageTitle(" ' . $title . '");
 </script>
 ';
 
 // Now important call to phpMyEdit
 
-new phpMyEdit($opts);
+new phpMyEdit_MultiQuery($opts);
 
 //eof
 
